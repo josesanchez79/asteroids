1c30dd26f23183b2e9f832adf7214c784fb49016
josesanchez79 <ironjo04@gmail.com>
2024-10-18 13:44:53 -0600

    added 60 fps lock for screen and added provided code by boot.dev

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
